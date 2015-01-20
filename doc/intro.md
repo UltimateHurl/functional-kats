3abe13f62a44ab62858ee05bb4bfc93540de8bb3
Eoin Hurrell <ultimatehurl@gmail.com>
2015-01-20 19:53:06 +0000

    Luhn checksum.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to fkats
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/functional-kats/core_test.clj
@@ -0,0 +1,7 @@
+(ns fkats.core-test
+  (:require [clojure.test :refer :all]
+            [fkats.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
